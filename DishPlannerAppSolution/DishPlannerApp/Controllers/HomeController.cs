32b5c551b368e5c3bf2febae98dc93d121b52386
Maryna Haiduk <maryna.i.haiduk@gmail.com>
2024-12-01 16:33:54 -0500

    Created Project - DishPlannerApp

---
+++ DishPlannerAppSolution/DishPlannerApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using DishPlannerApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DishPlannerApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
